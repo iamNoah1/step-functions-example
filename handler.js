a7c168afd5f0c5649d05971f313ec3168f7d2ba7
Noah Ispas <noahispas@gmail.com>
2017-10-23 21:43:07 +0200

    removes uneeded stuff, added second state alias lambda

--- handler.js
+++ handler.js
@@ -2,7 +2,15 @@
 
 module.exports.hello = (event, context, callback) => {
   callback(null, null);
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.ciao = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'ciao world!'
+    }),
+  };
+
+  callback(null, response);
 };
